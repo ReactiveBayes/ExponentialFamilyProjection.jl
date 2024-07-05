32249ca4e6ac57f7fae209b81e3a92d337ab8e2f
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-05 11:38:11 +0300

    add Binomial tests. Projection from Bernoulli to Binomial is not working

---
+++ test/projection/projected_to_binomial_tests.jl
@@ -0,0 +1,53 @@
+@testitem "Simple projection to `Binomial`" begin
+    using BayesBase, ExponentialFamily, Distributions, JET
+    using ExponentialFamilyProjection
+
+    include("./projected_to_setuptests.jl")
+
+    @testset let distribution = Binomial(10, 0.5)
+        @test test_projection_convergence(distribution)
+    end
+
+    @testset let distribution = Binomial(20, 0.1)
+        @test test_projection_convergence(distribution)
+    end
+
+    @testset let distribution = Binomial(2, 0.9) 
+        @test test_projection_convergence(distribution)
+    end
+
+
+end
+
+
+@testitem "Project `Poisson` to `Binomial`" begin
+    using BayesBase, ExponentialFamily, Distributions, JET
+    using ExponentialFamilyProjection
+
+    include("./projected_to_setuptests.jl")
+
+    @testset let distribution = Poisson(0.6)
+        @test test_projection_convergence(distribution, to = Binomial, conditioner = 5)
+    end
+
+    @testset let distribution = Poisson(5.3)
+        @test test_projection_convergence(distribution, to = Binomial, conditioner = 20)
+    end
+end
+
+
+
+@testitem "Project `Bernoulli` to `Binomial`" begin
+    using BayesBase, ExponentialFamily, Distributions, JET
+    using ExponentialFamilyProjection
+
+    include("./projected_to_setuptests.jl")
+
+    @testset let distribution = Bernoulli(0.6)
+        @test_broken test_projection_convergence(distribution, to = Binomial, conditioner = 3)
+    end
+
+    @testset let distribution = Bernoulli(0.3)
+        @test_broken test_projection_convergence(distribution, to = Binomial, conditioner = 3)
+    end
+end
