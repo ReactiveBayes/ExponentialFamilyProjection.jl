210aa72bb95312d25b3699bcdc02b3867daf63ae
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-05 13:59:44 +0300

    add projection tests for Weibull

---
+++ test/projection/projected_to_weibull_tests.jl
@@ -0,0 +1,52 @@
+@testitem "Simple projection to `Weibull`" begin
+    using BayesBase, ExponentialFamily, Distributions, JET
+    using ExponentialFamilyProjection
+
+    include("./projected_to_setuptests.jl")
+
+    @testset let distribution = Weibull()
+        @test test_projection_convergence(distribution)
+    end
+
+    @testset let distribution = Weibull(0.1, 2.0)
+        @test test_projection_convergence(distribution)
+    end
+
+    @testset let distribution = Weibull(3.14, 2.71)
+        @test test_projection_convergence(distribution)
+    end
+end
+
+#Because Weibull with k = 1 is Exponential distribution, we fix the conditioner to 1.
+@testitem "Project `Exponential` to `Weibull`" begin
+    using BayesBase, ExponentialFamily, Distributions, JET
+    using ExponentialFamilyProjection
+
+    include("./projected_to_setuptests.jl")
+
+    @testset let distribution = Exponential(0.1)
+        @test test_projection_convergence(
+            distribution,
+            to = Weibull,
+            conditioner = 1.0
+        )
+    end
+
+    @testset let distribution = Exponential(10.1)
+        @test test_projection_convergence(
+            distribution,
+            to = Weibull,
+            conditioner = 1.0
+        )
+    end
+
+    @testset let distribution = Exponential(100.32)
+        @test test_projection_convergence(
+            distribution,
+            to = Weibull,
+            conditioner = 1.0
+        )
+    end
+end
+
+#TODO: We should add a test for projecting Rayleigh into Weibull by fixing the conditioner to 2.
