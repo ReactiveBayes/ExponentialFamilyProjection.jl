24653b1e01ea6946eeb9c3fb689836eda2cf8f06
bvdmitri <bvdmitri@gmail.com>
2025-11-04 20:19:38 +0100

    set global seed for reproducibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,6 @@
-using ExponentialFamilyProjection, Test, ReTestItems
+using ExponentialFamilyProjection, Test, ReTestItems, Random
+
+# Set the random seed for reproducibility of kl-divergence tests
+Random.seed!(42)
 
 runtests(ExponentialFamilyProjection; memory_threshold = 1.0)
